4981b69f22b8b337ef6fa662f7ddb313b9d91080
Will Melbourne <willmelbourne@gmail.com>
2015-07-15 22:04:56 -0700

    added basic files to get react animate working

---
+++ server.php
@@ -0,0 +1,24 @@
+<?php
+$scriptInvokedFromCli =
+    isset($_SERVER['argv'][0]) && $_SERVER['argv'][0] === 'server.php';
+
+if($scriptInvokedFromCli) {
+    echo 'starting server on port 5000' . PHP_EOL;
+    exec('php -S localhost:5000 -t ./ server.php');
+} else {
+    return routeRequest();
+}
+
+function routeRequest()
+{
+
+    
+
+    switch($_SERVER["REQUEST_URI"]) {
+        case '/':
+            echo file_get_contents('./index.html');
+            break;
+     	default:
+            return false;
+        }
+}
