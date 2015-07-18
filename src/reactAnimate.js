1927b676f2e6008a9c27bb0f7784e08f11096236
Will Melbourne <willmelbourne@gmail.com>
2015-07-17 17:22:09 -0700

    make switch to reverse react js animation diretion

--- src/reactAnimate.js
+++ src/reactAnimate.js
@@ -2,12 +2,21 @@ var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var TodoList = React.createClass({
   getInitialState: function() {
-    return {items: ['hello', 'world', 'click', 'me']};
+    return {items: ['hello', 'world', 'click', 'me'],
+            goLeft : "true"};
   },
   handleAdd: function() {
     var newItems =
       this.state.items.concat([prompt('Enter some text')]);
     this.setState({items: newItems});
+  },
+  handleChangeDirection: function() {
+    if (this.state.goLeft == true) {
+      this.setState({goLeft: false})
+    }
+    else {
+      this.setState({goLeft: true})
+    }
   },
   handleRemove: function(i) {
     console.log("handleRemove " + i)
@@ -18,13 +27,29 @@ var TodoList = React.createClass({
   render: function() {
     var items = this.state.items.map(function(item, i) {
       return (
-        <div key={item} onClick={this.handleRemove.bind(this, i)}>
+        <div key={item} onClick={this.handleRemove.bind(this, i)} className="items">
           {item}
         </div>
       );
     }.bind(this));
+
+    if (this.state.goLeft) {
+      var left = "left"
+    }
+    else {
+      var left = "right"
+    }
+
+    var temp = React;
+    var cx = React.addons.classSet;
+    var classes = cx({
+      'oppositeDirection': this.state.goLeft
+    });
+    
     return (
-      <div>
+      <div className={classes} >
+        Direction = {left} 
+        <button onClick={this.handleChangeDirection}>Change Direction</button>
         <button onClick={this.handleAdd}>Add Item</button>
         <ReactCSSTransitionGroup transitionName="example">
           {items}
